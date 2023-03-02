9b944a4ddb67d32c337557187f798677f94cc9f1
“Ysachid48 <yosachid48@gmail.com>
2023-03-02 14:18:33 +0000

    hide show clock

---
+++ src/components/Clock/index.js
@@ -0,0 +1,36 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+  }
+
+  componentDidMount() {
+    console.log('Componet Mount Is Call')
+    this.timerID = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('Component Will Unmount')
+    clearInterval(this.timerID)
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  render() {
+    const {date} = this.state
+    console.log(date)
+
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
